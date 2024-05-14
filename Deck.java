e31ff80f1f9a76f72ae8bf401ed810cd5acbfbf3
Jacob1437 <106129480+Jacob1437@users.noreply.github.com>
2024-05-14 14:34:41 -0700

    Feat: shuffle, removeCard, createDeck
    added functions: shuffle, removeCard, createDeck to Deck class

--- Deck.java
+++ Deck.java
@@ -1,19 +1,33 @@
-import java.util.ArrayList;
+import java.util.*;
 public class Deck {
     // New instance for each game played
     ArrayList<Card> deck = new ArrayList<Card>();
     String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
-    
+
     public Deck() {
+        createDeck();
+        shuffleDeck();
+    }
+
+    public void createDeck() {
         for (String suit: suits) {
             for (int i = 0; i < 14; i++) {
                 deck.add(new Card(i, suit));
             }
-
         }
     }
 
     public void removeCard(int value, String suit) {
-        deck.remove()
+        for (int i = 0; i < deck.size(); i++) {
+            Card card = deck.get(i);
+            if (card.getValue() == value && card.getSuit().equals(suit)) {
+                deck.remove(i);
+                break; // dont need to look anymore
+            }
+        }
+    }
+
+    public void shuffleDeck() {
+        Collections.shuffle(deck);
     }
 }
