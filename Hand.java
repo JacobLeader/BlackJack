517e97cbc972ceb11adc926f87ca2d63a4a909c1
Jacob1437 <106129480+Jacob1437@users.noreply.github.com>
2024-05-14 16:27:07 -0700

    Create Hand.java

---
+++ Hand.java
@@ -0,0 +1,10 @@
+public class Hand {
+    // this represents each round of the game
+    public Hand(Deck deck) {
+        
+    }
+
+    // public void dealCards(int turn) {
+    //     if (card)
+    // }
+}
