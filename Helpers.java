9ed46dd0fb3cc813adaac66c6922a5c93087897f
Jacob1437 <106129480+Jacob1437@users.noreply.github.com>
2024-05-14 16:27:30 -0700

    Create Helpers.java
    Used right now only to get the hand value in player and dealer

---
+++ Helpers.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+public class Helpers {
+    public int getHandValue(ArrayList<Card> cards) {
+        // straight forward other than aces
+        int value = 0;
+        int aceCount = 0;
+        int tempValue;
+        for (Card card : cards) {
+            // value = (value + card.getValue() ? card.getValue() < 10  ? card.getValue() != 14 : aces++)
+            tempValue = card.getValue();
+            if (tempValue == 1) { // Ace
+                value += 11;
+                aceCount++;
+            } else if (tempValue >= 10) { // Face cards
+                value += 10;
+            } else {
+                value += tempValue;
+            }
+        }
+        // aces can be 1 or 11, so if value is above 21 & there are aces, than change aces from the initial value of 11 to 1 until under 21
+        while (value > 21 && aceCount > 0) {
+            value -= 10; // change 1 ace from 11 -> 1
+            aceCount--;
+        }
+        return value;
+    }
+}
