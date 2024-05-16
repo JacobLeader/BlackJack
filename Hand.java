0ee1ab9eca0a61f14b278850cd08277c32d0c56e
Jacob1437 <106129480+Jacob1437@users.noreply.github.com>
2024-05-16 02:55:57 -0700

    Feat: getCards, giveCard, getValue for dealer & player
    Hand should be used instead of player and dealer to keep track of cards.

--- Hand.java
+++ Hand.java
@@ -1,14 +1,16 @@
-public class Hand {
+import java.util.ArrayList;
+
+public class Hand extends Helpers {
     // this represents each round of the game
     // used to handle bets
     private int bet;
+    private ArrayList<Card> playerCards;
+    private ArrayList<Card> dealerCards;
     public Hand(int gBet) {
+        playerCards = new ArrayList<>();
+        dealerCards = new ArrayList<>();
         bet = gBet;
     }
-
-    // public void dealCards(int turn) {
-    //     if (card)
-    // }
 
     // adds or subtracts amount to change
     public void addToBet(int amount) {
@@ -21,5 +23,28 @@ public class Hand {
 
     public int getBet() {
         return bet;
+    }
+
+    public ArrayList<Card> getPlayerCards() {
+        return playerCards;
+    }
+
+    public ArrayList<Card> getDealerCards() {
+        return dealerCards;
+    }
+
+    public int getPlayerValue() {
+        return getHandValue(playerCards);
+    }
+
+    public int getDealerValue() {
+        return getHandValue(dealerCards);
+    }
+
+    public void givePlayerCard(Card card) {
+        playerCards.add(card);
+    }
+    public void giveDealerCard(Card card) {
+        dealerCards.add(card);
     }
 }
