cc4f6170a6cc4976f1c03f41f2424bdc46321751
Jacob <Jacob.leader06@gmail.com>
2024-06-10 05:07:10 -0700

    Feat: show Ascii Cards Horizontally!

--- Deck.java
+++ Deck.java
@@ -37,4 +37,87 @@ public class Deck {
         deck.add(card);
         return card;
     }
+
+    public static ArrayList<String> getCardLines(Card card) {
+        int value = card.getValue();
+        String suit = card.getSuit();
+        String symbol = getSuitSymbol(suit);
+        String cardValue = getValueAsString(value);
+
+        String cardTop = "┌─────────┐";
+        String cardBottom = "└─────────┘";
+
+        ArrayList<String> cardLines = new ArrayList<>();
+        cardLines.add(cardTop);
+
+        // top left number
+        String topLine = "│" + cardValue;
+        for (int i = 0; i < (9 - cardValue.length()); i++) {
+            topLine += " ";
+        }
+        topLine += "│";
+        cardLines.add(topLine);
+
+        cardLines.add("│         │");
+
+        // line w symbol
+        cardLines.add("│    " + symbol + "    │");
+
+        cardLines.add("│         │");
+
+        // bottom right number
+        String bottomLine = "│";
+        for (int i = 0; i < (9 - cardValue.length()); i++) {
+            bottomLine += " ";
+        }
+        bottomLine += cardValue + "│";
+        cardLines.add(bottomLine);
+
+        cardLines.add(cardBottom);
+
+        return cardLines;
+    }
+
+    public static String getValueAsString(int value) {
+        if (value == 1) {
+            return "A";
+        } else if (value == 11) {
+            return "J";
+        } else if (value == 12) {
+            return "Q";
+        } else if (value == 13) {
+            return "K";
+        } else {
+            return String.valueOf(value);
+        }
+    }
+
+    public static String getSuitSymbol(String suit) {
+        if ("Hearts".equals(suit)) {
+            return "♥";
+        } else if ("Diamonds".equals(suit)) {
+            return "♦";
+        } else if ("Clubs".equals(suit)) {
+            return "♣";
+        } else if ("Spades".equals(suit)) {
+            return "♠";
+        } else {
+            return " ";
+        }
+    }
+
+    public static void printCards(List<Card> cards) {
+        ArrayList<ArrayList<String>> allCardLines = new ArrayList<>();
+        for (Card card : cards) {
+            allCardLines.add(getCardLines(card));
+        }
+
+        // Go layer by layer down, building the cards of each list of lines
+        for (int i = 0; i < allCardLines.get(0).size(); i++) {
+            for (ArrayList<String> cardLines : allCardLines) {
+                System.out.print(cardLines.get(i) + " ");
+            }
+            System.out.println();
+        }
+    }
 }
